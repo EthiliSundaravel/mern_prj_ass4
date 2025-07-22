adbca275361f85ffda9cde5b8cdbb4c0ea2f7f5e
DESKTOP-RHMUDV9\Ethili Sundaravel <ethilispsdpe@gmail.com>
2025-07-21 23:16:44 -0400

    vercel error fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false, // Set to true if you want to bypass build errors (not recommended)
+  },
 };
 
 export default nextConfig;
